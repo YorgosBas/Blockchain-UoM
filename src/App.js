eae14b3811e67b4ad68fecba045dddd7e82069fb
YorgosBas <georgiosbas1@gmail.com>
2024-01-19 13:47:55 +0200

    Refactor app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import web3 from './web3';
+import lottery from './lottery';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    manager: '',
+    currentAccount: '',
+    message: '',
+    bids: {
+      Car: [],
+      Phone: [],
+      Computer: []
+    },
+    bidAmount: '',
+    contractBalance: ''
+  };
+
+  async componentDidMount() {
+    try {
+      const manager = await lottery.methods.manager().call();
+      const currentAccount = (await web3.eth.requestAccounts())[0];
+      const bids = {
+        Car: await lottery.methods.getBids('Car').call(),
+        Phone: await lottery.methods.getBids('Phone').call(),
+        Computer: await lottery.methods.getBids('Computer').call()
+      };
+      this.setState({ manager, currentAccount, bids });
+      if (!this.eventListenersSet) {
+        this.setupEventListeners();
+        this.eventListenersSet = true;
+      }
+        try {
+          const currentAccount = (await window.ethereum.request({ method: 'eth_requestAccounts' }))[0];
+          this.setState({ message: '', currentAccount });
+        } catch (error) {
+          this.setState({ message: 'Metamask has not connected yet' });
+        }
+    } catch (error) {
+    this.setState({ message: 'Metamask is not installed' });
+    }
+
+    this.updateInterval = setInterval(() => this.updateBidsAndBalance(), 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.updateInterval); // Clear interval on component unmount
+  }
+
+  updateBidsAndBalance = async () => {
+    const bids = {
+      Car: await lottery.methods.getBids('Car').call(),
+      Phone: await lottery.methods.getBids('Phone').call(),
+      Computer: await lottery.methods.getBids('Computer').call()
+    };
+    const contractBalance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ bids, contractBalance: web3.utils.fromWei(contractBalance, 'ether') });
+  };
+
+  setupEventListeners() {
+    window.ethereum.on('accountsChanged', (accounts) => {
+      const currentAccount = accounts[0];
+      this.setState({ currentAccount });
+    });
+
+    lottery.events.BidPlaced({
+        fromBlock: 'latest'
+    })
+    .on('data', (event) => {
+        console.log('Bid Event:', event);
+    })
+    .on('error', console.error);
+  }
+  
+  bidOnItem = async (item) => {
+    this.setState({ message: 'Placing your bid...' });
+    const bidAmountInWei = web3.utils.toWei("0.01", 'ether');
+
+    try {
+      await lottery.methods.bid(item).send({
+        from: this.state.currentAccount,
+        value: bidAmountInWei
+      });
+
+      this.setState({ message: 'Bid placed successfully!' });
+    } catch (error) {
+      this.setState({ message: 'Bid failed: ' + error.message });
+    }
+  };
+
+  pickWinners = async () => {
+    this.setState({ message: 'Picking winners...' });
+
+    try {
+      await lottery.methods.pickWinners().send({ from: this.state.currentAccount });
+      this.setState({ message: 'Winners have been picked!' });
+    } catch (error) {
+      this.setState({ message: 'Error: ' + error.message });
+    }
+  };
+
+  render() {
+    return (
+      <div className="text-center">
+        <h2>Lottery-Ballot</h2>
+        <div className="d-flex justify-content-center">
+          {['Car', 'Phone', 'Computer'].map(item => (
+            <div key={item} className="card mx-2" style={{ width: '18rem' }}>
+              <div className="card-body">
+                <h5 className="card-title">{item}</h5>
+                <p className="card-text">Bids: {this.state.bids[item].length}</p>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => this.bidOnItem(item)}
+                  disabled={this.state.currentAccount === this.state.manager}
+                >
+                  Bid
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="mt-4">
+          <button className="btn btn-warning mx-2" onClick={this.pickWinners}>Declare Winner</button>
+          <button className="btn btn-secondary mx-2" onClick={this.reveal}>Reveal</button>
+          <button className="btn btn-info mx-2" onClick={this.amIWinner}>Am I Winner</button>
+          <button className="btn btn-success mx-2" onClick={this.withdraw}>Withdraw</button>
+        </div>
+        <div className="mt-4">
+          <p>Current Account: {this.state.currentAccount}</p>
+          <p>Contract Owner: {this.state.manager}</p>
+          <p>Contract Balance: {this.state.contractBalance} ETH</p>
+        </div>
+        <div className="mt-4">
+          <h5>{this.state.message}</h5>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/lottery.js
@@ -0,0 +1,146 @@
+import web3 from './web3';
+const address = '0xd4AAa8351B9A56B2859c6012dE069fE820847D16';
+const abi = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "item",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "bidder",
+				"type": "address"
+			}
+		],
+		"name": "BidPlaced",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "item",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "winner",
+				"type": "address"
+			}
+		],
+		"name": "WinnerPicked",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "item",
+				"type": "string"
+			}
+		],
+		"name": "bid",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "bids",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "item",
+				"type": "string"
+			}
+		],
+		"name": "getBids",
+		"outputs": [
+			{
+				"internalType": "address payable[]",
+				"name": "",
+				"type": "address[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "items",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "manager",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "pickWinners",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+
+const lottery = new web3.eth.Contract(abi, address);
+
+export default lottery;
